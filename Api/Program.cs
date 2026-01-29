a5456b26b8fb10c659e02847032c84bd48faa7aa
Wessh <wesshbr@gmail.com>
2026-01-29 12:13:32 -0300

    feat(Api): ajustado a camada de api e adicionado o AppDbContext na Program da Camada Api.

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,11 +1,13 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(option => option.UseSqlite("Data Source: App.db"));
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-
-// Swagger
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
