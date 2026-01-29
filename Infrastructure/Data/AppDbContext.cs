b775308ff551723178288e7af29b1aafe2999743
Wessh <wesshbr@gmail.com>
2026-01-29 11:37:20 -0300

    feat(infrastructure): criado o AppDbContext para gerenciar TaskItem

---
+++ Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<TaskItem> TaskItems { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }
+    }
+}
