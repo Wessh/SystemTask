f25424a820cd95ba4e42e827c0c31bcd73624a9a
Wessh <wesshbr@gmail.com>
2026-01-31 00:57:17 -0300

    refac(Application): alterado o metodo GetByStatusAsync, recebia o parâmetro string e agora recebe StatusTask

--- Application/Interfaces/ITaskItemRepository.cs
+++ Application/Interfaces/ITaskItemRepository.cs
@@ -1,10 +1,11 @@
 ﻿using Domain.Entities;
+using Domain.Enums;
 
 namespace Application.Interfaces
 {
     public interface ITaskItemRepository
     {
-        public Task<IEnumerable<TaskItem>> GetByStatusAsync(string status);
+        public Task<IEnumerable<TaskItem>> GetByStatusAsync(StatusTask status);
         public Task<TaskItem?> GetByIdAsync(Guid id);
         public Task AddAsync(TaskItem task);
         public Task UpdateAsync(TaskItem task);
