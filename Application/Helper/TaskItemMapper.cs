85f202bdf639a767a3a6cc8819f8eb0a46d681f7
Wessh <wesshbr@gmail.com>
2026-02-02 17:12:53 -0300

    feat(application): criado uma classe Helper para fazer a conversão de entidade para dto e de dto para entidade

---
+++ Application/Helper/TaskItemMapper.cs
@@ -0,0 +1,26 @@
+﻿using Application.Dtos;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Helper
+{
+    internal static class TaskItemMapper
+    {
+        public static TaskItemDto ToDto(TaskItem taskItem) =>
+            new TaskItemDto(
+                taskItem.Id,
+                taskItem.Title!,
+                taskItem.Description!,
+                taskItem.Status,
+                taskItem.DueDate
+            );
+
+        public static TaskItem ToEntity(CreateTaskItemDto dto) =>
+            new TaskItem(dto.Title, dto.Description, dto.DueDate);
+    }
+
+}
