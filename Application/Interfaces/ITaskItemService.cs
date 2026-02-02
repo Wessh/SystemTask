0294077f85a8ca72275666bbec44ba1a5f5b0463
Wessh <wesshbr@gmail.com>
2026-02-02 15:49:42 -0300

    feat(application): criado e documentado a interface de service ITaskItemService

---
+++ Application/Interfaces/ITaskItemService.cs
@@ -0,0 +1,51 @@
+﻿using Application.Dtos;
+using Domain.Enums;
+
+namespace Application.Interfaces;
+public interface ITaskItemService
+{
+    /// <summary>
+    /// Adiciona uma nova tarefa no sistema.
+    /// </summary>
+    /// <param name="dto">Objeto contendo título, descrição e prazo da tarefa.</param>
+    /// <returns>Retorna o DTO da tarefa criada, incluindo ID e status inicial.</returns>
+    public Task<TaskItemDto> AddAsync(CreateTaskItemDto dto);
+    /// <summary>
+    /// Obtém uma tarefa pelo id informado.
+    /// </summary>
+    /// <param name="id">Identificador da tarefa.</param>
+    /// <returns>Retorna um DTO de tarefa ou null se não encontrar nada.</returns>
+    public Task<TaskItemDto> GetByIdAsync(Guid id);
+    /// <summary>
+    /// Obtém tarefas pelo Status informado.
+    /// </summary>
+    /// <param name="status">Status da tarefa (InProgress, Completed, etc.).</param>
+    /// <returns>Lista de DTOs das tarefas encontradas.</returns>
+    public Task<IEnumerable<TaskItemDto>> GetByStatusAsync(StatusTask status);
+
+    /// <summary>
+    /// Inicia a execução de uma tarefa.
+    /// </summary>
+    /// <param name="id">Identificador da tarefa.</param>
+    /// <returns>Retorna o DTO atualizado da tarefa.</returns>
+    public Task<TaskItemDto> StartAsync(Guid id);
+    /// <summary>
+    /// Coloca a tarefa em espera.
+    /// </summary>
+    /// <param name="id">Identificador da tarefa.</param>
+    /// <returns>Retorna o DTO atualizado da tarefa.</returns>
+    public Task<TaskItemDto> OnHoldAsync(Guid id);
+    /// <summary>
+    /// Marca a tarefa como concluida.
+    /// </summary>
+    /// <param name="id">Identificador da tarefa.</param>
+    /// <returns>Retorna o DTO atualizado da tarefa.</returns>
+    public Task<TaskItemDto> CompleteAsync(Guid id);
+    /// <summary>
+    /// Cancela a tarefa.
+    /// </summary>
+    /// <param name="id">Identificador da tarefa.</param>
+    /// <returns>Retorna o DTO atualizado da tarefa.</returns>
+    public Task<TaskItemDto> CancelAsync(Guid id);
+}
+
