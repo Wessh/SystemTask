eb0b1f74934de37d2f15d6788f5a9eef0cc4e870
Wessh <wesshbr@gmail.com>
2026-02-04 17:01:07 -0300

    feat(api): criado os metodos REST para TaskItemController, ajustado a Connection string para ser chamada a partir de appsettings, adicionado a injeção de depêndencia e adicionado Serializer para enums.

---
+++ Api/Controllers/TaskItemController.cs
@@ -0,0 +1,105 @@
+﻿using Application.Dtos;
+using Application.Interfaces;
+using Domain.Enums;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskItemController : ControllerBase
+    {
+        private readonly ITaskItemService _service;
+
+        public TaskItemController(ITaskItemService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("create")]
+        public async Task<ActionResult<TaskItemDto>> Post([FromBody] CreateTaskItemDto dto) 
+        {
+            if (dto.Title is null)
+                return BadRequest("Title não pode ser nulo.");
+            if (dto.DueDate <= DateTime.UtcNow)
+                return BadRequest("DueDate não pode ser menor que a data atual.");
+
+            var taskItem = await _service.AddAsync(dto);
+
+            if(taskItem is null)
+                return BadRequest("Erro ao criar a tarefa.");
+
+            return CreatedAtAction(nameof(GetById), new {id = taskItem.Id }, taskItem);
+        }
+
+        [HttpGet("{id}")]
+        
+        public async Task<ActionResult<TaskItemDto>> GetById(Guid id)
+        {
+            var taskItem = await _service.GetByIdAsync(id);
+
+            if (taskItem is null)
+                return NotFound($"Id {id}, não encontrado!");
+
+            else
+                return Ok(taskItem);
+        }
+
+        [HttpGet("status/{status}")]
+        public async Task<ActionResult<IEnumerable<TaskItemDto>>> GetByStatus(StatusTask status)
+        {
+            var taskItems = await _service.GetByStatusAsync(status);
+            return Ok(taskItems);
+        }
+
+        [HttpPut("{id}/start")]
+        public async Task<ActionResult> Start(Guid id)
+        {
+            if (id == Guid.Empty)
+                return BadRequest("Id não informado");
+
+            var taskItem = await _service.StartAsync(id);
+            if(taskItem is null)
+                return NotFound($"Id {id}, não encontrado!");
+            return Ok(taskItem);
+        }
+
+        [HttpPut("{id}/on-hold")]
+        public async Task<ActionResult> OnHold(Guid id)
+        {
+            if (id == Guid.Empty)
+                return BadRequest("Id não informado");
+
+            var taskItem = await _service.OnHoldAsync(id);
+            if (taskItem is null)
+                return NotFound($"Id {id}, não encontrado!");
+            return Ok(taskItem);
+        }
+
+        [HttpPut("{id}/complete")]
+        public async Task<ActionResult> Complete(Guid id)
+        {
+            if (id == Guid.Empty)
+                return BadRequest("Id não informado");
+
+            var taskItem = await _service.CompleteAsync(id);
+            if (taskItem is null)
+                return NotFound($"Id {id}, não encontrado!");
+            return Ok(taskItem);
+        }
+
+        [HttpPut("{id}/cancel")]
+        public async Task<ActionResult> Cancel(Guid id)
+        {
+            if (id == Guid.Empty)
+                return BadRequest("Id não informado");
+
+            var taskItem = await _service.CancelAsync(id);
+            if (taskItem is null)
+                return NotFound($"Id {id}, não encontrado!");
+            return Ok(taskItem);
+        }
+
+    }
+}
