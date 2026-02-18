687dec33d16c530ddc446cf9d9b9e1b28efdd7ad
Wessh <wesshbr@gmail.com>
2026-02-18 15:09:02 -0300

    feat(api): adicionado o ExceptionMiddleware para tratar das excessões que podem ser apresentadas

---
+++ Api/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+
+namespace Api.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch(ArgumentException ex)
+            {
+                await HandleExceptionAsync(context, HttpStatusCode.BadRequest, ex.Message);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                await HandleExceptionAsync(context, HttpStatusCode.NotFound, ex.Message);
+            }
+            catch(InvalidOperationException ex) 
+            {
+                await HandleExceptionAsync(context, HttpStatusCode.Conflict, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, HttpStatusCode.InternalServerError, "Ocorreu um erro no servidor.");
+            }
+        }
+        public static async Task HandleExceptionAsync(HttpContext context, HttpStatusCode status, string message)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)status;
+             
+            var errorResponse = new { error = message };
+            var json = JsonSerializer.Serialize(errorResponse);
+
+            await context.Response.WriteAsync(json);
+        }
+    }
+}
